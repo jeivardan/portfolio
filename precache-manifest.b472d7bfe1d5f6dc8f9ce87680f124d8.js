274c3622e23133adae82a6670053dc64ce58720c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-26 14:35:57 +0000

    Deploy jeivardan/portfolio to github.com/jeivardan/portfolio.git:gh-pages

---
+++ precache-manifest.b472d7bfe1d5f6dc8f9ce87680f124d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7306f712cf204ed95ec2a98cdafaffce",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e073e9e98237bf849c0f",
+    "url": "/portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a04e264f40df6de992d9",
+    "url": "/portfolio/static/css/main.50af8f0c.chunk.css"
+  },
+  {
+    "revision": "e073e9e98237bf849c0f",
+    "url": "/portfolio/static/js/2.2edfeab6.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/portfolio/static/js/2.2edfeab6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a04e264f40df6de992d9",
+    "url": "/portfolio/static/js/main.81ea47ca.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
